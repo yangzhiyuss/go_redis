0a4b11d4bdbf81863d12357630fec668e17114b3
杨志宇 <yangzy22091405@q1.com>
2022-12-09 10:26:38 +0800

    新增文件

--- src/adlist.go
+++ src/adlist.go
@@ -8,22 +8,22 @@ const (
 )
 
 // 双端链表节点
-type ListNode struct {
-	pre   *ListNode
-	next  *ListNode
+type listNode struct {
+	pre   *listNode
+	next  *listNode
 	value interface{}
 }
 
 // 双端链表迭代器
 type listIter struct {
-	Next      *ListNode
-	Direction int
+	next      *listNode
+	direction int
 }
 
 // 双端链表结构
 type List struct {
-	head  *ListNode
-	tail  *ListNode
+	head  *listNode
+	tail  *listNode
 	dup   func(ptr interface{}) interface{}
 	match func(ptr interface{}, key interface{}) int
 	len   uint64
@@ -34,23 +34,23 @@ func (list *List) Length() uint64 {
 	return list.len
 }
 
-func (list *List) First() *ListNode {
+func (list *List) First() *listNode {
 	return list.head
 }
 
-func (list *List) Last() *ListNode {
+func (list *List) Last() *listNode {
 	return list.tail
 }
 
-func (listNode *ListNode) PreNode() *ListNode {
+func (listNode *listNode) PreNode() *listNode {
 	return listNode.pre
 }
 
-func (listNode *ListNode) NextNode() *ListNode {
+func (listNode *listNode) NextNode() *listNode {
 	return listNode.next
 }
 
-func (listNode *ListNode) NodeValue() interface{} {
+func (listNode *listNode) NodeValue() interface{} {
 	return listNode.value
 }
 
@@ -72,7 +72,7 @@ func (list *List) GetMatchMethod() func(ptr interface{}, key interface{}) int {
 
 // 给链表表头增加新节点
 func (list *List) AddNodeHead(value interface{}) {
-	node := new(ListNode)
+	node := new(listNode)
 	node.value = value
 
 	if list.len == 0 {
@@ -91,7 +91,7 @@ func (list *List) AddNodeHead(value interface{}) {
 
 // 给链表表尾增加新节点
 func (list *List) AddNodeTail(value interface{}) {
-	node := new(ListNode)
+	node := new(listNode)
 	node.value = value
 	if list.len == 0 {
 		list.head = node
@@ -104,14 +104,14 @@ func (list *List) AddNodeTail(value interface{}) {
 		list.tail.next = node
 		list.tail = node
 	}
-	list.len ++
+	list.len++
 }
 
-//在链表中插入数据
-//0:向前插入节点
-//1:向后插入节点
-func (list *List) InsertNode(old_node *ListNode, value interface{}, after int) {
-	node := new(ListNode)
+// 在链表中插入数据
+// 0:向前插入节点
+// 1:向后插入节点
+func (list *List) InsertNode(old_node *listNode, value interface{}, after int) {
+	node := new(listNode)
 	node.value = value
 	if after == 1 {
 		node.pre = old_node
@@ -135,7 +135,7 @@ func (list *List) InsertNode(old_node *ListNode, value interface{}, after int) {
 	list.len++
 }
 
-func (list *List) DelNode(node *ListNode)  {
+func (list *List) DelNode(node *listNode) {
 	if node.pre != nil {
 		node.pre = node.next
 	} else {
@@ -156,3 +156,127 @@ func (list *List) DelNode(node *ListNode)  {
 	list.len--
 }
 
+func (list *List) GetIterator(direction int) *listIter {
+	iter := new(listIter)
+
+	if direction == AL_START_HEAD {
+		iter.next = list.head
+	} else {
+		iter.next = list.tail
+	}
+
+	iter.direction = direction
+	return iter
+}
+
+func (li *listIter) RewindHead(list *List) {
+	li.next = list.head
+	li.direction = AL_START_HEAD
+}
+
+func (li *listIter) RewindTail(list *List) {
+	li.next = list.tail
+	li.direction = AL_START_TAIL
+}
+
+func (iter *listIter) Next() *listNode {
+	current := iter.next
+
+	if current != nil {
+		if iter.direction == AL_START_HEAD {
+			iter.next = current.next
+		} else {
+			iter.next = current.pre
+		}
+	}
+
+	return current
+}
+
+func (list *List) Dup() *List {
+	//初始化
+	copyList := new(List)
+	copyList.dup = list.dup
+	copyList.match = list.match
+	//获取iter
+	iter := list.GetIterator(AL_START_HEAD)
+	//进行遍历
+	for node := iter.Next(); node != nil; node = iter.Next() {
+		var value interface{}
+		if copyList.dup != nil {
+			value = copyList.dup(node.value)
+			if value == nil {
+				return nil
+			}
+		} else {
+			value = node.value
+		}
+		//添加节点
+		copyList.AddNodeTail(value)
+	}
+
+	return copyList
+}
+
+func (list *List) SearchKey(key interface{}) *listNode {
+	iter := list.GetIterator(AL_START_HEAD)
+	for node := iter.Next(); node != nil; node = iter.Next() {
+		if list.match != nil {
+			if list.match(node.value, key) > 0 {
+				return node
+			}
+		} else {
+			if node.value == key {
+				return node
+			}
+		}
+	}
+	return nil
+}
+
+/*
+ * 返回链表在给定索引上的值。
+ *
+ * 索引以 0 为起始，也可以是负数， -1 表示链表最后一个节点，诸如此类。
+ *
+ * 如果索引超出范围（out of range），返回 NULL 。
+ *
+ * T = O(N)
+ */
+func (list *List) Index(index int64) *listNode {
+	var node *listNode
+	if index < 0 {
+		index = (-index) - 1
+		node = list.tail
+		for index > 0 && node != nil {
+			node = node.pre
+			index--
+		}
+	} else {
+		node = list.head
+		for index > 0 && node != nil {
+			node = node.next
+			index--
+		}
+	}
+
+	return node
+}
+
+/*
+取出链表的表尾节点，并将它移动到表头，成为新的表头节点。
+*/
+func (list *List) Rotate() {
+	tail := list.tail
+	if list.len <= 1 {
+		return
+	}
+	//取出表尾元素
+	list.tail = tail.pre
+	list.tail.next = nil
+	//插入到表头
+	list.head.pre = tail
+	tail.pre = nil
+	tail.next = list.head
+	list.head = tail
+}
